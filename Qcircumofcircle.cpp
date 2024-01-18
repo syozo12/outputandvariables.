61552d4ff6adbb7d4c0ded643540e08264f19856
syozo12 <156905994+syozo12@users.noreply.github.com>
2024-01-18 15:26:38 +0530

    Add files via upload

---
+++ Qcircumofcircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    printf("enter the radius:");
+    cin>>r;
+    float p=2*3.14*r;
+    printf("circumference of circle is:%f",p);
+    return 0;
+
+
+}
